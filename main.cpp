bbebe643a6cba7a66034af56fd1633cfd95d4431
astricka1337 <algirdasstricka@gmail.com>
2026-03-09 10:42:19 +0200

    Apsirasiau valiutas

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
-
+#include <iomanip>
+#include <string>
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
+    // Valiutų kursai:
+    double gbp_bendras = 0.8729, gbp_pirkti = 0.8600, gbp_parduoti = 0.9220;
+    double usd_bendras = 1.1793, usd_pirkti = 1.1460, usd_parduoti = 1.2340;
+    double inr_bendras = 104.6918, inr_pirkti = 101.3862, inr_parduoti = 107.8546;
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
     auto lang = "C++";
     std::cout << "Hello and welcome to " << lang << "!\n";
